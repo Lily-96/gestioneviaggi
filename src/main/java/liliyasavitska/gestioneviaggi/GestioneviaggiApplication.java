d2dfee00df2d499bedb202f407efb68b34b57298
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-07-25 10:59:18 +0200

    impostazione cartelle

--- src/main/java/liliyasavitska/gestioneviaggi/GestioneviaggiApplication.java
+++ src/main/java/liliyasavitska/gestioneviaggi/GestioneviaggiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GestioneviaggiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestioneviaggiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestioneviaggiApplication.class, args);
+    }
 
 }
