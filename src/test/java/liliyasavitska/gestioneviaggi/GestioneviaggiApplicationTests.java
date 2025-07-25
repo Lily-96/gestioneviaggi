6dbc70c62c3bcc3a9bfa43b395f290560f827252
Lily-96 <LILIYASAVITSKA@GMAIL.COM>
2025-07-25 09:41:18 +0200

    creazione progetto

---
+++ src/test/java/liliyasavitska/gestioneviaggi/GestioneviaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package liliyasavitska.gestioneviaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneviaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
